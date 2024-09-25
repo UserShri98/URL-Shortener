f7e58fbac61b17d20b39aeff94b7bb5c3d02e7c3
UserShri98 <sjc3098@gmail.com>
2024-09-25 17:33:00 +0530

    added the frontend part

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const urlRoute=require('./routes/url')
 const URL=require('./models/url')
+const staticRoute=require('./routes/staticRoutes')
+const path=require('path')
 const {connectMongoDb}=require("./connect")
 
 const app=express();
@@ -12,9 +14,14 @@ connectMongoDb("mongodb://localhost:27017/short-url")
 .then(()=>console.log("Mongodb is connected"))
 
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use('/url',urlRoute)
+app.get('/', staticRoute);
 
-app.get('/:shortId',async (req,res)=>{
+app.set("view engine","ejs");
+app.set("views",path.resolve('./views'))
+
+app.get('/url/:shortId',async (req,res)=>{
     const shortId=req.params.shortId;
     const entry= await URL.findOneAndUpdate(
         {
---
+++ routes/staticRoutes.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const URL = require('../models/url');
+
+
+
+const router=express.Router();
+
+
+router.get('/',async(req,res)=>{
+    const allURLs= await URL.find({})
+    res.render("home",{
+     urls:allURLs,
+    })
+})
+
+module.exports=router;
